36b4e271b898afb2427c2fb30eedada374a5eb21
Aliakbar Kadkhoda <aliakbarkadkhoda137818@hotmail.com>
2025-03-07 14:03:08 +0330

    Update AddToCart.tsx

--- src/page-components/products/addToCart/AddToCart.tsx
+++ src/page-components/products/addToCart/AddToCart.tsx
@@ -1,28 +1,33 @@
 "use client";
 
-
 import { setToCart } from "@/redux/slicers/useSlice";
 import { RootState } from "@/redux/store";
 import { useDispatch, useSelector } from "react-redux";
 
-
 interface IProps {
-  id: number
+  id: number;
 }
-const AddToCart = (props :IProps) => {
-  const cart = useSelector((state:RootState)=> state.userSlice.cart)
-  const name = useSelector((state:RootState)=> state.auth.name)
+const AddToCart = (props: IProps) => {
+  const cart = useSelector((state: RootState) => state.userSlice.cart);
+  const name = useSelector((state: RootState) => state.auth.name);
 
-  const {id} = props
-  const dispatch =  useDispatch()
-  return (<>
-      {
-      name == "" ? <span className="text-3xl text-warning font-bold border p-10 text-center">You Have To Loggin First</span> :   <button  onClick={()=> dispatch(setToCart(id))} className={`btn ${cart.includes(id) ? "btn-error": "btn-success"}`}>{cart.includes(id) ? "Remove From Cart" : "Add To Cart"}</button>
-    }
-  
-  </>
-
-
+  const { id } = props;
+  const dispatch = useDispatch();
+  return (
+    <>
+      {name == "" ? (
+        <span className="text-3xl text-warning font-bold border p-10 text-center">
+          You Have To Loggin First
+        </span>
+      ) : (
+        <button
+          onClick={() => dispatch(setToCart(id))}
+          className={`btn ${cart.includes(id) ? "btn-error" : "btn-success"}`}
+        >
+          {cart.includes(id) ? "Remove From Cart" : "Add To Cart"}
+        </button>
+      )}
+    </>
   );
 };
 
