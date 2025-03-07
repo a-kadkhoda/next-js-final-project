4ce6e3fb6a761c563d32192aea51982ec4f45913
Aliakbar Kadkhoda <aliakbarkadkhoda137818@hotmail.com>
2025-03-07 14:02:30 +0330

    Update AddToCart.tsx

--- src/page-components/products/addToCart/AddToCart.tsx
+++ src/page-components/products/addToCart/AddToCart.tsx
@@ -10,12 +10,18 @@ interface IProps {
   id: number
 }
 const AddToCart = (props :IProps) => {
-  const state = useSelector((state:RootState)=> state.userSlice.cart)
-  console.log(state);
+  const cart = useSelector((state:RootState)=> state.userSlice.cart)
+  const name = useSelector((state:RootState)=> state.auth.name)
+
   const {id} = props
   const dispatch =  useDispatch()
-  return (
-    <button  onClick={()=> dispatch(setToCart(id))} className={`btn ${state.includes(id) ? "btn-error": "btn-success"}`}>{state.includes(id) ? "Remove From Cart" : "Add To Cart"}</button>
+  return (<>
+      {
+      name == "" ? <span className="text-3xl text-warning font-bold border p-10 text-center">You Have To Loggin First</span> :   <button  onClick={()=> dispatch(setToCart(id))} className={`btn ${cart.includes(id) ? "btn-error": "btn-success"}`}>{cart.includes(id) ? "Remove From Cart" : "Add To Cart"}</button>
+    }
+  
+  </>
+
 
   );
 };
